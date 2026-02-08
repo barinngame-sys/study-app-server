e4a7dab1c337569f9b3c369756e7a289337261d3
barinngame-sys <barinngame-sys@example.com>
2026-02-08 21:58:34 +0900

    ポート設定を修正

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 3000;
+// 👇 Renderが決めたポートを使う。なければ3000を使う。
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
